14fbbcb871a140e7bad4c20262c87d3288cc9d57
Navpreet Gill <navgillst@gmail.com>
2022-10-30 11:14:15 +0000

    Commit changes

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+        addition();
+
+    }
+
+
+        public static void addition ()
+        {
+            int a = 5, b = 5, sum;
+            sum = a + b;
+            System.out.println(sum);
+
+        }
+
+}
